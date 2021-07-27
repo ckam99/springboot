94945631e2346861763f6cba72674dad6ff1f456
Ckam <ckam225@gmail.com>
2021-07-27 14:41:28 -0700

    siwtch from gradle to maven project

--- demo/src/main/java/com/example/demo/http/exception/EntityAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.http.exception;
-
-public class EntityAlreadyExistException extends Exception {
-
-    public EntityAlreadyExistException(String message) {
-        super(message);
-    }
-}
