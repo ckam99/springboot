27c0ee2d99fe270093aae844967c5176b6209ac2
Ckam <ckam225@gmail.com>
2021-07-28 10:12:13 -0700

    reset version

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Todo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
